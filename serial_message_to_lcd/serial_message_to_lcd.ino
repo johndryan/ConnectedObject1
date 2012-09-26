c236cbff26c4df35c24ebb84cf1b0133fe3d54cd
John Ryan <john@lifeforms.ie>
2012-09-26 14:52:36 -0700

    Check URL repeatedly and only read message

--- serial_message_to_lcd/serial_message_to_lcd.ino
+++ serial_message_to_lcd/serial_message_to_lcd.ino
@@ -7,6 +7,10 @@ byte mac[] = {  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };  // Enter a MAC address fo
 char serverName[] = "ve.lifeforms.ie";
 EthernetClient client;                   // Initialize the Ethernet client library
 
+unsigned long lastConnectionTime = 0;          // last time you connected to the server, in milliseconds
+boolean lastConnected = false;                 // state of the connection last time through the main loop
+const unsigned long postingInterval = 10*1000;  // delay between updates, in milliseconds
+
 int  val = 0;
 char message[61];
 int messageBytesRead = 0;
@@ -15,7 +19,7 @@ String trimmedMessage = "";
 boolean messagePresent = false;
 
 void setup() { 
-  Serial.begin(9600);                    // RFID reader SOUT pin connected to Serial RX pin at 2400bps
+  Serial.begin(9600);
 
   lcd.begin(16, 2);                      // set up the LCD's number of columns and rows
   lcd.setCursor(0, 0);
@@ -29,22 +33,9 @@ void setup() {
   }
   // give the Ethernet shield a second to initialize:
   delay(1000);
+  
   lcd.clear();
   lcd.print("Connecting...");
-
-  // if you get a connection, report back via serial: 
-  if (client.connect(serverName, 80)) {
-    lcd.clear();
-    lcd.print("Connected");
-    // Make a HTTP request:
-    client.println("GET /arduino.txt HTTP/1.0");
-    client.println();
-  } 
-  else {
-    // if you didn't get a connection to the server:
-    lcd.clear();
-    lcd.println("connection failed");
-  }
 }  
 
 void loop() {
@@ -52,21 +43,53 @@ void loop() {
   // from the server, read them and print them:
   if (client.available()) {
     char c = client.read();
-    if (c == '\r') {
-      lcd.clear();
-      lcd.setCursor(0,0);
+    Serial.print(c);
+    if (c == 126) {
+      if (messagePresent) {
+        messagePresent = false;
+      } else {
+        lcd.clear();
+        lcd.setCursor(0,0);
+        messagePresent = true; 
+      }
     } else {
-      if (c != '\n') lcd.print(c);
+      if (messagePresent) lcd.print(c);
     }
   }
 
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting.");
+  // if there's no net connection, but there was one last time
+  // through the loop, then stop the client:
+  if (!client.connected() && lastConnected) {
     client.stop();
-
-    // do nothing forevermore:
-    while(true);
   }
-} 
+
+  // if you're not connected, and ten seconds have passed since
+  // your last connection, then connect again and send data:
+  if(!client.connected() && (millis() - lastConnectionTime > postingInterval)) {
+    httpRequest();
+  }
+  // store the state of the connection for next time through
+  // the loop:
+  lastConnected = client.connected();
+}
+
+
+// this method makes a HTTP connection to the server:
+void httpRequest() {
+  // if there's a successful connection:
+  if (client.connect(serverName, 80)) {
+    client.println("GET /arduino.txt HTTP/1.0");
+    client.println();
+
+    // note the time that the connection was made:
+    lastConnectionTime = millis();
+  } 
+  else {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(" - CONNECTION - ");
+    lcd.setCursor(1,0);
+    lcd.print("   - ERROR -    ");
+    client.stop();
+  }
+}
